b622f4fd563aa16bc15ece5681ed23816e2f7c86
Sharon Wu <sharonwu827@gmail.com>
2021-10-20 12:36:33 -0700

    Update

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+
+monthly_challenges = {
+    "january":"No Carbs!",
+    'february':"Drink more water",
+    'march':"Smile and Be Happy"
+}
+
 # Create your views here.
+
+def monthly_challenge_by_number(request,month):
+    return HttpResponse(month)
+
 def monthly_challenge(request,month):
-    challenge_text = None
-    if month =='january':
-        challenge_text = "No Carbs!"
-    elif month =='february':
-        challenge_text = "Drink more water"
-    elif month =='march':
-        challenge_text = "Smile and Be Happy"
-    else:
-        return HttpResponseNotFound("Not Supported")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("Not Found")
+
